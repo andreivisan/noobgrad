454078a2256269e1bd7675e5fe6e3a6c1599ab58
andreivisan <andrei.m.visan@gmail.com>
2023-03-27 20:37:14 +0200

    First commit

---
+++ activations.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+'''
+SIGMOID
+'''
+# Define the sigmoid activation function
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def logistic_regression_derivatives(X, y, w, b):
+    samples_size = X.shape[0]
+    
+    # Compute predicted probabilities
+    p = sigmoid(np.dot(X, w) + b)
+    
+    # Compute the derivatives(gradients) for w and b
+    dW = (1 / samples_size) * np.dot(X.T, (p - y))
+    db = (1 / samples_size) * np.sum(p - y)
+    
+    return dW, db
+
+def train_logistic_regression(X, y, learning_rate, epochs):
+    samples_size, features_size = X.shape
+    
+    # Initialize the weights and bias
+    W = np.zeros((features_size, 1))
+    b = 0
+    
+    for epoch in range(epochs):
+        # Compute the derivatives(gradients) for w and b
+        dW, db = logistic_regression_derivatives(X, y, W, b)
+        
+        # Update the weights and bias
+        W -= learning_rate * dW
+        b -= learning_rate * db
+        
+        # Optional: Compute and print loss every 10 epochs
+        # if epoch % 10 == 0:
+        #     p = sigmoid(np.dot(X, W) + b)
+        #     loss = -1 / n_samples * np.sum(y * np.log(p) + (1 - y) * np.log(1 - p))
+        #     print(f'Epoch {epoch}/{n_epochs}, Loss: {loss}')
+        
+    return W, b
---
+++ nn.py
@@ -0,0 +1,33 @@
+import numpy as np
+import activations
+
+class DenseLayer:
+    def __init__(self, n_inputs, n_neurons, activation):
+        '''
+        This line of code initializes the weights using a technique called "He initialization" 
+        (also known as "He normal initialization"), which is named after Kaiming He, 
+        who proposed it in the 2015 paper 
+        Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
+        '''
+        self.weights = np.random.randn(n_inputs, n_neurons) * np.sqrt(2 / n_inputs)
+        self.bias = np.zeros((1, n_neurons))
+        self.activation = activation
+        
+    def forward(self, inputs):
+        if self.activation == 'sigmoid':
+            return activations.sigmoid(np.dot(inputs, self.weights) + self.bias)
+        else:
+            raise ValueError(f'Unsupported activation: {self.activation}')
+        
+class NeuralNetwork:
+    def __init__(self, layers):
+        self.layers = layers
+        
+    def forward(self, inputs):
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+    
+    def train(self, X, y, learning_rate, epochs):
+        if self.layers[0].activation == 'sigmoid':
+            pass
