7d735ef0d64b3acd71b7883d708368b0931faae2
andreivisan <andrei.m.visan@gmail.com>
2023-03-28 08:53:20 +0200

    Added linear regression

--- activations.py
+++ activations.py
@@ -1,22 +1,15 @@
 import numpy as np
 
 
-def z(X, W, b):
+def f_x(X, W, b):
     return np.dot(X, W) + b
 
-'''
-SIGMOID
-'''
-# Define the sigmoid activation function
-def sigmoid(X, W, b):
-    f_x = z(X, W, b)
-    return 1 / (1 + np.exp(-f_x))
 
-def logistic_regression_derivatives(X, y, w, b):
+def compute_gradients(X, y, w, b, model):
     samples_size = X.shape[0]
     
-    # Compute predicted probabilities
-    p = sigmoid(X,w,b)
+    # Compute predicted values
+    p = model
     
     # Compute the derivatives(gradients) for w and b
     dW = (1 / samples_size) * np.dot(X.T, (p - y))
@@ -24,16 +17,13 @@ def logistic_regression_derivatives(X, y, w, b):
     
     return dW, db
 
-def train_logistic_regression(X, y, learning_rate, epochs):
-    samples_size, features_size = X.shape
-    
-    # Initialize the weights and bias
-    W = np.zeros((features_size, 1))
-    b = 0
+
+def train_model(X, y, W, b, learning_rate, epochs, model) :
+    samples_size = X.shape[0]
     
     for epoch in range(epochs):
         # Compute the derivatives(gradients) for w and b
-        dW, db = logistic_regression_derivatives(X, y, W, b)
+        dW, db = compute_gradients(X, y, W, b, model)
         
         # Update the weights and bias
         W -= learning_rate * dW
@@ -46,3 +36,40 @@ def train_logistic_regression(X, y, learning_rate, epochs):
             print(f'Epoch {epoch}/{epochs}, Loss: {loss}')
         
     return W, b
+
+
+'''
+LINEAR REGRESSION
+'''
+def linear(X, W, b):
+    return f_x(X, W, b)
+
+def linear_regression_derivatives(X, y, w, b):
+    return compute_gradients(X, y, w, b, f_x(X, w, b))
+
+def train_linear_regression(X, y, learning_rate, epochs):
+    features_size = X.shape[1]
+    # Initialize the weights and bias
+    W = np.zeros((features_size, 1))
+    b = 0
+    return train_model(X, y, W, b, learning_rate, epochs, f_x(X, W, b))
+
+
+'''
+SIGMOID
+'''
+# Define the sigmoid activation function
+def sigmoid(X, W, b):
+    z = f_x(X, W, b)
+    return 1 / (1 + np.exp(-z))
+
+# Derivatives are also called gradients
+def logistic_regression_derivatives(X, y, w, b):
+    return compute_gradients(X, y, w, b, sigmoid(X, w, b))
+
+def train_logistic_regression(X, y, learning_rate, epochs):
+    features_size = X.shape[1]
+    # Initialize the weights and bias
+    W = np.zeros((features_size, 1))
+    b = 0
+    return train_model(X, y, W, b, learning_rate, epochs, sigmoid(X, W, b))
--- nn.py
+++ nn.py
@@ -9,12 +9,14 @@ class DenseLayer:
         who proposed it in the 2015 paper 
         Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification
         '''
-        self.weights = np.random.randn(n_inputs, n_neurons) * np.sqrt(2 / n_inputs)
+        self.weights = np.random.randn(n_inputs, n_neurons) * np.sqrt(2.0 / n_inputs)
         self.bias = np.zeros((1, n_neurons))
         self.activation = activation
         
     def forward(self, inputs):
-        if self.activation == 'sigmoid':
+        if self.activation == 'linear':
+            return activations.linear(inputs, self.weights, self.bias)
+        elif self.activation == 'sigmoid':
             return activations.sigmoid(inputs, self.weights, self.bias)
         else:
             raise ValueError(f'Unsupported activation: {self.activation}')
@@ -29,7 +31,12 @@ class NeuralNetwork:
         return inputs
     
     def train(self, X, y, learning_rate, epochs):
-        if self.layers[0].activation == 'sigmoid':
+        if len(self.layers) != 1:
+            raise ValueError("Training is only supported for single-layer networks.")
+        
+        if self.layers[0].activation == 'linear':
+            self.layers[0].weights, self.layers[0].bias = activations.train_linear_regression(X, y, learning_rate, epochs)
+        elif self.layers[0].activation == 'sigmoid':
             self.layers[0].weights, self.layers[0].bias = activations.train_logistic_regression(X, y, learning_rate, epochs)
             
 
@@ -41,3 +48,9 @@ if __name__ == "__main__":
     nn = NeuralNetwork([DenseLayer(2, 1, activation='sigmoid')])
     nn.train(X, y, learning_rate=0.01, epochs=100)
     predictions = nn.forward(X)
+    
+    y_linear = 2 * X[:, 0:1] + 3 * X[:, 1:2] + np.random.randn(100, 1) * 0.1
+
+    nn_linear = NeuralNetwork([DenseLayer(2, 1, activation='linear')])
+    nn_linear.train(X, y_linear, learning_rate=0.01, epochs=100)
+    predictions_linear = nn_linear.forward(X)
